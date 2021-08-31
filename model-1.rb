f65c97c14fc3b3da7888cdf830af180e15a94daa
Kevin Gloss <kgloss89@gmail.com>
2021-08-30 20:58:47 -0600

    Add hash data type

--- model-1.rb
+++ model-1.rb
@@ -1,11 +1,14 @@
-book = "Way of Kings"
-book_1 = "Name of the Wind"
+book = "Way of Kings";
+book_1 = "Name of the Wind";
 
-page = 5
-page_turn = 1.0
+page = 5;
+page_turn = 1.0;
 
-good = true
-okay = false
+good = true;
+okay = false;
 
-books_read = [book, book_1]
-pages_read = [72, 300]
+books_read = [book, book_1];
+pages_read = [72, 300];
+
+books_enjoyed = { 1 => book, 2 => book_1};
+book_ratings = {1 => "Bad", 2 => "Not Good", 3 => "Mediocre", 4 => "Enjoyable", 5 => "So Good!"};
